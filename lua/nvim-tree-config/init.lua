07088f7add65ab367e907fc5bfddbdeeab32d6eb
srTlakuache <calloman123@gmail.com>
2022-11-30 13:51:56 -0600

    Fixed tree

--- lua/nvim-tree-config/init.lua
+++ lua/nvim-tree-config/init.lua
@@ -2,5 +2,10 @@ require("nvim-tree").setup({
     sort_by = "case_sensitive",
     view = {
         adaptive_size = true,
+    },
+    actions = {
+        open_file = {
+            quit_on_open = true,
+        }
     }
 })
