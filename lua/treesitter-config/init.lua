3d4f30c4f80b1ce8087aa48d1902beca946c2ea0
Claudio Tlakuache <claussielTlakuache@gmail.com>
2022-11-30 14:10:29 -0600

    Everything is okay

--- lua/treesitter-config/init.lua
+++ lua/treesitter-config/init.lua
@@ -1,36 +1,38 @@
-local status, ts = pcall(require, 'nvim-treesitter.configs')
-if(not status) then return end
+local status, ts = pcall(require, "nvim-treesitter.configs")
+if not status then
+	return
+end
 
-ts.setup{
-    ensure_installed = {
-    "c",
-    "lua",
-    "javascript",
-    "python",
-    "html",
-    "tsx",
-    "json",
-    "css",
-    "rasi",
-    },
-    sync_install = true,
-    auto_install = true,
+ts.setup({
+	ensure_installed = {
+		"c",
+		"lua",
+		"javascript",
+		"python",
+		"html",
+		"tsx",
+		"json",
+		"css",
+		"rasi",
+	},
+	sync_install = true,
+	auto_install = true,
 
-    highlight = {
-       enable = true,
-       disable = {},
-    },
+	highlight = {
+		enable = true,
+		disable = {},
+	},
 
-    indent = {
-        enable = true,
-        disable = {},
-    },
+	indent = {
+		enable = true,
+		disable = {},
+	},
 
-    autotag = {
-        enable = true,
-    },
-    additional_vim_regex_highlighting = {"rasi"},
-}
+	autotag = {
+		enable = true,
+	},
+	additional_vim_regex_highlighting = { "rasi" },
+})
 
-local parse_config = require "nvim-treesitter.parsers".get_parser_configs()
-parse_config.tsx.filetype_to_parsername = {"javascript","typescript.tsx"}
+local parse_config = require("nvim-treesitter.parsers").get_parser_configs()
+parse_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
